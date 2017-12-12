93145484b65c44f85f592e73765a25ae513d3533
vaibhav28398 <31373963+vaibhav28398@users.noreply.github.com>
2017-12-12 12:06:09 +0530

    Added Project of web-development made in technical fest of MNNIT Allahabad

---
+++ cvmaker/GeneratedPdf/PDF_Genrator.js
@@ -0,0 +1,38 @@
+﻿var
+    form,
+ a4 = [595.28, 841.89]; // for a4 size paper width and height
+$(document).ready(function () {
+    $(".create_pdf").on("click", function (e) {
+        $('body').scrollTop(0);
+        var selector = $(e.target).attr('myprint');
+        createPDF(selector);
+    });
+});
+
+function createPDF(selector) {
+    form = $(selector);
+    getCanvas().then(function (canvas) {
+        var cache_width = form.width()
+
+        var
+         img = canvas.toDataURL("image/png"),
+         doc = new jsPDF({
+             unit: 'px',
+             format: 'a4'
+         });
+        doc.addImage(img, 'JPEG', 20, 20);
+        doc.save('techumber-html-to-pdf.pdf');
+        form.width(cache_width);
+    });
+}
+
+// create canvas object
+function getCanvas() {
+
+    form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');
+    return html2canvas(form, {
+        imageTimeout: 2000,
+        removeContainer: true
+    });
+}
+
